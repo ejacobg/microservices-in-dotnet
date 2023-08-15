acb13a1785a82231c09881f06430928fa9f3d2f5
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-14 18:56:04 -0700

    Added shopping cart controller and data store interface

---
+++ ShoppingCart/Controllers/ShoppingCartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("/shoppingcart")] 
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IShoppingCartStore _shoppingCartStore;
+
+        public ShoppingCartController(IShoppingCartStore shoppingCartStore)
+        {
+            _shoppingCartStore = shoppingCartStore;
+        }
+
+        // Objects (like ShoppingCart) will be serialized to JSON before being returned in the response.
+        [HttpGet("{userId:int}")]
+        public ShoppingCart Get(int userId) =>
+            _shoppingCartStore.Get(userId);
+    }
+}
---
+++ ShoppingCart/ShoppingCartStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCart
+{
+    // IShoppingCartStore provides methods for working with shopping carts from a data store.
+    public interface IShoppingCartStore
+    {
+        // Get returns the shopping cart for the given user, creating one if it doesn't exist.
+        ShoppingCart Get(int userId);
+        
+        // Save saves the given shopping cart to the data store.
+        void Save(ShoppingCart shoppingCart);
+    }
+
+    // ShoppingCartStore provides an in-memory data store for shopping carts.
+    public class ShoppingCartStore : IShoppingCartStore
+    {
+        private static readonly Dictionary<int, ShoppingCart> Database = new Dictionary<int, ShoppingCart>();
+
+        public ShoppingCart Get(int userId) =>
+            Database.ContainsKey(userId) ? Database[userId] : new ShoppingCart(userId);
+
+        public void Save(ShoppingCart shoppingCart) =>
+            Database[shoppingCart.UserId] = shoppingCart;
+    }
+}
