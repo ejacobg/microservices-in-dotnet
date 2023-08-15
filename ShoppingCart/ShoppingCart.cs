653061c7a3a6d396106a536f28834feb03cc1eb2
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-14 17:35:43 -0700

    Added ShoppingCart domain model

---
+++ ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCart
+{
+    public class ShoppingCart
+    {
+        private readonly HashSet<ShoppingCartItem> items = new();
+        public int UserId { get; }
+        public IEnumerable<ShoppingCartItem> Items => items;
+        public ShoppingCart(int userId) => UserId = userId;
+
+        public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItems)
+        {
+            foreach (var item in shoppingCartItems)
+                items.Add(item);
+        }
+
+        public void RemoveItems(int[] productCatalogueIds) =>
+            items.RemoveWhere(i => productCatalogueIds.Contains(i.ProductCatalogueId));
+    }
+
+    public record ShoppingCartItem(
+        int ProductCatalogueId,
+        string ProductName,
+        string Description,
+        Money Price)
+    {
+        public virtual bool Equals(ShoppingCartItem? obj) =>
+            obj != null && ProductCatalogueId.Equals(obj.ProductCatalogueId);
+
+        public override int GetHashCode() =>
+            ProductCatalogueId.GetHashCode();
+    }
+
+    public record Money(string Currency, decimal Amount);
+}
