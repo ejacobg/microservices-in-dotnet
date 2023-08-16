7cc88110551df20ec38f88ab03c3f3ecbfc0aaf6
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-15 22:01:28 -0700

    Using Polly for retrying, added annotations to Startup.cs

--- ShoppingCart/Startup.cs
+++ ShoppingCart/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
+using ShoppingCart.Models;
 
 namespace ShoppingCart
 {
@@ -15,11 +17,23 @@ namespace ShoppingCart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Use the Scrutor package to automatically bind implementations to their corresponding interfaces.
             services.Scan(selector =>
                 selector
                     .FromAssemblyOf<Startup>()
-                    .AddClasses()
+                    // Copied from original repo. Not exactly sure what the implementation type filter does, but it is needed to run the project.
+                    .AddClasses(c => c.Where(t => t != typeof(ProductCatalogClient) && t.GetMethods().All(m => m.Name != "<Clone>$")))
                     .AsImplementedInterfaces());
+
+            // Inject a typed HTTP client into our ProductCatalogClient.
+            services.AddHttpClient<IProductCatalogClient, ProductCatalogClient>()
+                // Use the Polly package to automatically retry failed requests.
+                .AddTransientHttpErrorPolicy(policy =>
+                    // Set up a retry policy with exponential backoff (each waiting period is twice as long as the last).
+                    policy.WaitAndRetryAsync(
+                        3,
+                        attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt))));
         }
 
         public void Configure(IApplicationBuilder app)
