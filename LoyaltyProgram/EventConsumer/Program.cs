1d3e4bef28fec717ad41104ed24d0aede82a82d8
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-18 12:06:15 -0700

    Bootstrapped event consumer application

---
+++ LoyaltyProgram/EventConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
