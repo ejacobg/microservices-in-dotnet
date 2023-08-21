60fbebd25232f6f9c9f20601062ed409f22aaf00
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-21 15:45:25 -0700

    Updated IShoppingCartStore interface

--- ShoppingCart/Models/ShoppingCartStore.cs
+++ ShoppingCart/Models/ShoppingCartStore.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
 
 namespace ShoppingCart.Models
 {
@@ -6,21 +10,24 @@ namespace ShoppingCart.Models
     public interface IShoppingCartStore
     {
         // Get returns the shopping cart for the given user, creating one if it doesn't exist.
-        ShoppingCart Get(int userId);
-        
+        Task<ShoppingCart> Get(int userId);
+
         // Save saves the given shopping cart to the data store.
-        void Save(ShoppingCart shoppingCart);
+        Task Save(ShoppingCart shoppingCart);
     }
 
-    // ShoppingCartStore provides an in-memory data store for shopping carts.
-    public class ShoppingCartStore : IShoppingCartStore
+    // InmemShoppingCartStore provides an in-memory data store for shopping carts.
+    public class InmemShoppingCartStore : IShoppingCartStore
     {
         private static readonly Dictionary<int, ShoppingCart> Database = new Dictionary<int, ShoppingCart>();
 
-        public ShoppingCart Get(int userId) =>
-            Database.ContainsKey(userId) ? Database[userId] : new ShoppingCart(userId);
+        public Task<ShoppingCart> Get(int userId) =>
+            Task.FromResult(Database.ContainsKey(userId) ? Database[userId] : new ShoppingCart(userId));
 
-        public void Save(ShoppingCart shoppingCart) =>
+        public Task Save(ShoppingCart shoppingCart)
+        {
             Database[shoppingCart.UserId] = shoppingCart;
+            return Task.CompletedTask;
+        }
     }
 }
