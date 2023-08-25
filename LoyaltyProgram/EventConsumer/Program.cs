993cef50c883114f5b28a5f217be3d08b2bcb6f0
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-24 18:48:07 -0700

    Refactored event consumer into standalone class, added system test

--- LoyaltyProgram/EventConsumer/Program.cs
+++ LoyaltyProgram/EventConsumer/Program.cs
@@ -5,36 +5,57 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-var start = await GetStartIdFromDatastore(); // Continue from where we left off last time.
-const long numEvents = 100L;
-var client = new HttpClient();
-
-// Prepare and send the request.
-client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-using var resp =
-    await client.GetAsync(new Uri($"http://special-offers:5002/events?start={start}&end={start + numEvents}"));
-
-// Process the events that were returned.
-await ProcessEvents(await resp.Content.ReadAsStreamAsync());
-
-// Save the starting point for the next batch of events.
-await SaveStartIdToDataStore(start); // The value of `start` gets updated during the ProcessEvents() call.
-
-// fake implementation. Should get from a real database
-Task<long> GetStartIdFromDatastore() => Task.FromResult(0L);
-
-// fake implementation. Should apply business rules to events
-async Task ProcessEvents(Stream content)
+namespace EventConsumer
 {
-    // Deserialization doesn't seem to work correctly. Getting default values for all the fields. Will not be fixing this for now.
-    var events = await JsonSerializer.DeserializeAsync<SpecialOfferEvent[]>(content) ?? Array.Empty<SpecialOfferEvent>();
-    foreach (var @event in events)
+    public class Program
     {
-        Console.WriteLine(@event);
-        start = Math.Max(start, @event.SequenceNumber + 1);
+        public static async Task Main(string[] args)
+        {
+            var start = await GetStartIdFromDatastore(); // Continue from where we left off last time.
+            var end = 100;
+            var specialOffersHostName = args.Length >= 1 ? args[0] : "http://special-offers:5002";
+            var notificationsHostName = args.Length >= 2 ? args[1] : "http://notifications:5003";
+
+            start = await EventConsumer.ConsumeBatch(start, end, specialOffersHostName, notificationsHostName);
+
+            // Save the starting point for the next batch of events.
+            await SaveStartIdToDataStore(start); // The value of `start` gets updated during the ProcessEvents() call.
+        }
+
+        // fake implementation. Should get from a real database
+        public static Task<long> GetStartIdFromDatastore() => Task.FromResult(0L);
+
+        public static Task SaveStartIdToDataStore(long startId) => Task.CompletedTask;
     }
+
+    public static class EventConsumer
+    {
+        public static async Task<long> ConsumeBatch(long start, long end, string specialOffersHostName,
+            string notificationsHostName)
+        {
+            var client = new HttpClient();
+
+            // Prepare and send the request.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using var resp =
+                await client.GetAsync(new Uri($"{specialOffersHostName}/events?start={start}&end={end}"));
+
+            // Process the events that were returned.
+            // Deserialization doesn't seem to work correctly. Getting default values for all the fields. Will not be fixing this for now.
+            var events =
+                await JsonSerializer.DeserializeAsync<SpecialOfferEvent[]>(await resp.Content.ReadAsStreamAsync()) ??
+                Array.Empty<SpecialOfferEvent>();
+
+            foreach (var @event in events)
+            {
+                // Match special offer in @event to registered users and send notification to matching user.
+                await client.PostAsync($"{notificationsHostName}/notify", new StringContent(""));
+                start = Math.Max(start, @event.SequenceNumber + 1);
+            }
+
+            return start;
+        }
+    }
+
+    public record SpecialOfferEvent(long SequenceNumber, DateTimeOffset OccuredAt, string Name, object Content);
 }
-
-Task SaveStartIdToDataStore(long startId) => Task.CompletedTask;
-
-public record SpecialOfferEvent(long SequenceNumber, DateTimeOffset OccuredAt, string Name, object Content);
---
+++ LoyaltyProgram/ServiceTests/Scenarios/RegisterAndGetNotification.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LoyaltyProgram;
+using LoyaltyProgram.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using ServiceTests.Mocks;
+using Xunit;
+
+namespace ServiceTests.Scenarios
+{
+    public class RegisterUserAndGetNotification : IDisposable
+    {
+        private static int _mocksPort = 5050;
+        private readonly MocksHost _serviceMock;
+        private readonly IHost _loyaltyProgramHost;
+        private readonly HttpClient _sut;
+
+        public RegisterUserAndGetNotification()
+        {
+            _serviceMock = new MocksHost(_mocksPort); // Launch our mocks.
+            _loyaltyProgramHost = new HostBuilder() // Run our loyalty program service.
+                .ConfigureWebHost(x => x
+                    .UseStartup<Startup>()
+                    .UseTestServer())
+                .Start();
+            _sut = _loyaltyProgramHost.GetTestClient();
+        }
+
+        [Fact]
+        public async Task Scenario()
+        {
+            await RegisterNewUser();
+            await RunConsumer();
+            AssertNotificationWasSent();
+        }
+
+        private async Task RegisterNewUser()
+        {
+            var actual = await _sut.PostAsync( // Register a new user.
+                "/users",
+                new StringContent(
+                    JsonSerializer.Serialize(new User(0, "Chr", 0, new UserSettings())),
+                    Encoding.UTF8,
+                    "application/json"));
+
+            Assert.Equal(HttpStatusCode.Created, actual.StatusCode);
+        }
+
+        private Task RunConsumer()
+        {
+            return EventConsumer.EventConsumer.ConsumeBatch(
+                0,
+                100,
+                $"http://localhost:{_mocksPort}/specialoffers",
+                $"http://localhost:{_mocksPort}"
+            );
+        }
+
+        private void AssertNotificationWasSent() => Assert.True(NotificationsMock.ReceivedNotification);
+
+        // Apparently the test still hangs after completion. Not sure why this is.
+        public void Dispose()
+        {
+            _serviceMock?.Dispose();
+            _loyaltyProgramHost?.Dispose();
+            _sut?.Dispose();
+        }
+    }
+}
