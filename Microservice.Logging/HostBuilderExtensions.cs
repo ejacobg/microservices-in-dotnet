dcf4ed3c5c6a374631a663ccd377928549fcd4f0
Ean Jacob Gayban <ejacobg@outlook.com>
2023-08-28 14:25:44 -0700

    Added logging package

---
+++ Microservice.Logging/HostBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Formatting.Json;
+
+namespace Microservice.Logging
+{
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// UseLogging configures Serilog as the logger and adds the trace ID to each log.
+        /// Use this method on the host builder in your Program.cs file.
+        /// Package this library using the following command:
+        ///     dotnet pack --configuration Release
+        /// </summary>
+        public static IHostBuilder UseLogging(this IHostBuilder builder) =>
+            builder.UseSerilog((context, logger) => // Configure Serilog as the logger.
+            {
+                logger
+                    .Enrich.FromLogContext() // Add metadata to logs.
+                    .Enrich.WithSpan();
+                if (context.HostingEnvironment.IsDevelopment())
+                    logger.WriteTo.ColoredConsole(
+                        // Add tracing information when developing.
+                        outputTemplate:
+                        "{Timestamp:yyyy-MM-dd HH:mm:ss} {TraceId} {Level:u3} {Message}{NewLine}{Exception}");
+                else
+                    logger.WriteTo.Console(new JsonFormatter()); // Use JSON logs in production.
+            });
+    }
+}
